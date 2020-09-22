203f5916f03ef0685063c96b1d6114ae906479d8
Manuel Villagran <aludor96@gmail.com>
2020-09-22 16:27:02 -0600

    Autenticacion Facebook

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:proyecto_administracion/login/login_screen.dart';
+import 'package:proyecto_administracion/login/login_screen2.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Login(),
+      home: Login2(),
     );
   }
 }
