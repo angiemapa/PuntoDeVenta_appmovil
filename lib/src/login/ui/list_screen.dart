1b822d93425b79608af5b06420415e10b3458be5
Manuel Villagran <aludor96@gmail.com>
2020-10-27 23:57:04 -0600

    Pantalla Tipo de producto

--- lib/src/login/ui/list_screen.dart
+++ lib/src/login/ui/list_screen.dart
@@ -30,6 +30,66 @@ class _ListState extends State<List> {
           ),
         ],
       ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: GridView.builder(
+                itemCount: 6,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 20,
+                  crossAxisSpacing: 20,
+                  childAspectRatio: 0.75,
+                ),
+                itemBuilder: (context, index) => ItemCard(),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ItemCard extends StatelessWidget {
+  const ItemCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.all(10.0),
+            height: 180,
+            width: 160,
+            decoration: BoxDecoration(
+              color: Colors.blue[300],
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Image.asset('assets/images/facebook.png'),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 7.0),
+          child: Text(
+            'Facebook',
+            style: TextStyle(color: Colors.grey),
+          ),
+        ),
+        Text(
+          'Precio \$\$\$',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ],
     );
   }
 }
