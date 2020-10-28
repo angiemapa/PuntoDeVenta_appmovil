fc9777df68e111e2a458c892a536480be75e4e0c
Manuel Villagran <aludor96@gmail.com>
2020-10-28 02:32:37 -0600

    Lista de pedidos

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:proyecto_administracion/src/login/ui/buy_list_screen.dart';
 import 'package:proyecto_administracion/src/routes/app_route.dart';
 
 void main() {
@@ -9,10 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      home: BuyList(),
+      /*debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       initialRoute: "Login",
-      routes: getApplicationRoutes(),
+      routes: getApplicationRoutes(),*/
     );
   }
 }
