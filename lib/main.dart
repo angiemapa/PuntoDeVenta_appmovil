7ddf7e3947894bf50c2b996db78e4ee3bd9d348c
Manuel Villagran <aludor96@gmail.com>
2020-11-10 22:07:32 -0600

    Redes Sociales

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:proyecto_administracion/src/login/ui/login_screen.dart';
+import 'package:proyecto_administracion/login/login_screen3.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -14,7 +17,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: LoginScreen(),
+      home: Correo(),
     );
   }
 }
